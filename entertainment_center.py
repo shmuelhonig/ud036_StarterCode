79b0f55f82132dd55fc8b0a3234d48fedc6daa86
Shmuel Honig <shonig@gmail.com>
2017-10-24 21:33:38 -0400

    Add entertainment_center.py file from IPND project

---
+++ entertainment_center.py
@@ -0,0 +1,162 @@
+import fresh_tomatoes
+import media
+
+beautiful_mind = media.Movie("A Beautiful Mind",
+                             "Drama",
+                             "2001",
+                             "https://images-na.ssl-images-amazon.com/images/M/MV5BMzcwYWFkYzktZjAzNC00OGY1LWI4YTgtNzc5MzVjMDVmNjY0XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY1000_CR0,0,673,1000_AL_.jpg",
+                             "https://www.youtube.com/watch?v=YWwAOutgWBQ")
+
+few_good_men = media.Movie("A Few Good Men",
+                           "Drama",
+                           "1992",
+                           "https://images-na.ssl-images-amazon.com/images/M/MV5BMmRlZDQ1MmUtMzE2Yi00YTkxLTk1MGMtYmIyYWQwODcxYzRlXkEyXkFqcGdeQXVyNTI4MjkwNjA@._V1_.jpg",
+                           "https://www.youtube.com/watch?v=4AC0wWxadvc")
+
+dodgeball = media.Movie("Dodgeball: A True Underdog Story",
+                        "Comedy",
+                        "2004",
+                        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTIwMzE2MjM4MV5BMl5BanBnXkFtZTYwNjA1OTY3._V1_.jpg",
+                        "https://www.youtube.com/watch?v=W-XbDZUnUmw")
+
+ex_machina = media.Movie("Ex Machina",
+                         "Science Fiction",
+                         "2014",
+                         "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUxNzc0OTIxMV5BMl5BanBnXkFtZTgwNDI3NzU2NDE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+                         "https://www.youtube.com/watch?v=EoQuVnKhxaM")
+
+ferris_bueller = media.Movie("Ferris Bueller's Day Off",
+                             "Comedy",
+                             "1986",
+                             "https://images-na.ssl-images-amazon.com/images/M/MV5BMDA0NjZhZWUtNmI2NC00MmFjLTgwZDYtYzVjZmNhMDVmOTBkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
+                             "https://www.youtube.com/watch?v=K-X2XzKqBiE")
+
+field_of_dreams = media.Movie("Field of Dreams",
+                              "Drama",
+                              "1989",
+                              "https://images-na.ssl-images-amazon.com/images/M/MV5BNzk5OWY0YjAtYWU3ZS00Y2Q4LWFlNjItMzgwMTQ2MjIyMDFmL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY1000_CR0,0,665,1000_AL_.jpg",
+                              "https://www.youtube.com/watch?v=Ut06d4dptWo" )
+
+finding_nemo = media.Movie("Finding Nemo",
+                           "Animation",
+                           "2003",
+                           "https://images-na.ssl-images-amazon.com/images/M/MV5BZjMxYzBiNjUtZDliNC00MDAyLTg3N2QtOWNjNmNhZGQzNDg5XkEyXkFqcGdeQXVyNjE2MjQwNjc@._V1_.jpg",
+                           "https://www.youtube.com/watch?v=wZdpNglLbt8")
+
+gattaca = media.Movie("Gattaca",
+                      "Science Fiction",
+                      "1997",
+                      "https://images-na.ssl-images-amazon.com/images/M/MV5BNDQxOTc0MzMtZmRlOS00OWQ5LWI2ZDctOTAwNmMwOTYxYzlhXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg",
+                      "https://www.youtube.com/watch?v=hWjlUj7Czlk")
+
+gladiator = media.Movie("Gladiator",
+                        "Action",
+                        "2000",
+                        "https://images-na.ssl-images-amazon.com/images/M/MV5BMDliMmNhNDEtODUyOS00MjNlLTgxODEtN2U3NzIxMGVkZTA1L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,675,1000_AL_.jpg",
+                        "https://www.youtube.com/watch?v=0BLZbrLogTo")
+
+good_will_hunting = media.Movie("Good Will Hunting",
+                                "Drama",
+                                "1997",
+                                "https://images-na.ssl-images-amazon.com/images/M/MV5BOTI0MzcxMTYtZDVkMy00NjY1LTgyMTYtZmUxN2M3NmQ2NWJhXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY1000_CR0,0,655,1000_AL_.jpg",
+                                "https://www.youtube.com/watch?v=PaZVjZEFkRs")
+
+goodfellas = media.Movie("Goodfellas",
+                         "Crime Drama",
+                         "1990",
+                         "https://images-na.ssl-images-amazon.com/images/M/MV5BNThjMzczMjctZmIwOC00NTQ4LWJhZWItZDdhNTk5ZTdiMWFlXkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SY1000_CR0,0,669,1000_AL_.jpg",
+                         "https://www.youtube.com/watch?v=qo5jJpHtI1Y")
+
+jurassic_park = media.Movie("Jurassic Park",
+                            "Adventure",
+                            "1993",
+                            "https://images-na.ssl-images-amazon.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_.jpg",
+                            "https://www.youtube.com/watch?v=_IesovZQR4g")
+
+cousin_vinny = media.Movie("My Cousin Vinny",
+                           "Comedy",
+                           "1992",
+                           "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQxNDYzMTg1M15BMl5BanBnXkFtZTgwNzk4MDgxMTE@._V1_SY1000_CR0,0,673,1000_AL_.jpg",
+                           "https://www.youtube.com/watch?v=SL4HdaZXuOw")
+
+no_country = media.Movie("No Country for Old Men",
+                         "Crime Drama",
+                         "2007",
+                         "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA5Njk3MjM4OV5BMl5BanBnXkFtZTcwMTc5MTE1MQ@@._V1_.jpg",
+                         "https://www.youtube.com/watch?v=38A__WT3-o0")
+
+raising_arizona = media.Movie("Raising Arizona",
+                              "Comedy",
+                              "1987",
+                              "https://images-na.ssl-images-amazon.com/images/M/MV5BOGUwZTEwZTYtZmQ0OS00NmM1LWE2ZTQtNjZiYmE4ZmUxMjk1XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SY1000_SX664_AL_.jpg",
+                              "https://www.youtube.com/watch?v=2AIfVoGUs6c")
+
+road_to_perdition = media.Movie("Road to Perdition",
+                                "Crime Drama",
+                                "2002",
+                                "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA0MDM4ODM0Ml5BMl5BanBnXkFtZTYwNDE3MjU3._V1_.jpg",
+                                "https://www.youtube.com/watch?v=k1iCd___dNY")
+
+prince_of_thieves = media.Movie("Robin Hood: Prince of Thieves",
+                                "Action",
+                                "1991",
+                                "https://images-na.ssl-images-amazon.com/images/M/MV5BODM3MjA3NDM3M15BMl5BanBnXkFtZTgwMDc1NjUwMjI@._V1_.jpg",
+                                "https://www.youtube.com/watch?v=DJSsMR5NUuc")
+
+rounders = media.Movie("Rounders",
+                       "Drama",
+                       "1998",
+                       "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc4OTcxNDY2Nl5BMl5BanBnXkFtZTgwNDg0MzkxMDE@._V1_.jpg",
+                       "https://www.youtube.com/watch?v=9r-K5dmt0Rc")
+
+rudy = media.Movie("Rudy",
+                   "Drama",
+                   "1993",
+                   "https://images-na.ssl-images-amazon.com/images/M/MV5BZGUzMDU1YmQtMzBkOS00MTNmLTg5ZDQtZjY5Njk4Njk2MmRlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_.jpg",
+                   "https://www.youtube.com/watch?v=eDKOlH0I0nQ")
+
+lotr = media.Movie("The Lord of the Rings",
+                   "Adventure",
+                   "2001-2003",
+                   "https://images-na.ssl-images-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SY999_CR0,0,673,999_AL_.jpg",
+                   "https://www.youtube.com/watch?v=-JNVtorI3vw")
+
+princess_bride = media.Movie("The Princess Bride",
+                             "Adventure",
+                             "1987",
+                             "https://images-na.ssl-images-amazon.com/images/M/MV5BMGM4M2Q5N2MtNThkZS00NTc1LTk1NTItNWEyZjJjNDRmNDk5XkEyXkFqcGdeQXVyMjA0MDQ0Mjc@._V1_SY1000_CR0,0,676,1000_AL_.jpg",
+                             "https://www.youtube.com/watch?v=WNNUcHRiPS8")
+
+terminator = media.Movie("The Terminator",
+                         "Science Fiction",
+                         "1984",
+                         "https://images-na.ssl-images-amazon.com/images/M/MV5BODE1MDczNTUxOV5BMl5BanBnXkFtZTcwMTA0NDQyNA@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+                         "https://www.youtube.com/watch?v=k64P4l2Wmeg")
+
+truman_show = media.Movie("The Truman Show",
+                          "Drama",
+                          "1998",
+                          "https://images-na.ssl-images-amazon.com/images/M/MV5BMDIzODcyY2EtMmY2MC00ZWVlLTgwMzAtMjQwOWUyNmJjNTYyXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SY1000_SX674_AL_.jpg",
+                          "https://www.youtube.com/watch?v=loTIzXAS7v4")
+
+usual_suspects = media.Movie("The Usual Suspects",
+                             "Mystery",
+                             "1995",
+                             "https://images-na.ssl-images-amazon.com/images/M/MV5BYTViNjMyNmUtNDFkNC00ZDRlLThmMDUtZDU2YWE4NGI2ZjVmXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,670,1000_AL_.jpg",
+                             "https://www.youtube.com/watch?v=oiXdPolca5w")
+
+spinal_tap = media.Movie("This is Spinal Tap",
+                         "Comedy",
+                         "1984",
+                         "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ2MTIzMzg5Nl5BMl5BanBnXkFtZTgwOTc5NDI1MDE@._V1_.jpg",
+                         "https://www.youtube.com/watch?v=z6F0VyKp4Ik")
+
+movies = [beautiful_mind, few_good_men, dodgeball, ex_machina, ferris_bueller,
+          field_of_dreams, finding_nemo, gattaca, gladiator, good_will_hunting,
+          goodfellas, jurassic_park, cousin_vinny, no_country, raising_arizona,
+          road_to_perdition, prince_of_thieves, rounders, rudy, lotr,
+          princess_bride, terminator, truman_show, usual_suspects, spinal_tap]
+
+# Call the function, defined in fresh_tomatoes.py, that generates the html file
+# that displays the given list of movies
+fresh_tomatoes.open_movies_page(movies)
