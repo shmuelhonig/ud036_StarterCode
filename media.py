a69c34da30bf80e897d078c5db62b43e85dffa26
Shmuel Honig <shonig@gmail.com>
2017-10-27 13:54:09 -0400

    Update style to conform with PEP8

--- media.py
+++ media.py
@@ -1,12 +1,16 @@
 import webbrowser
 
+
 class Movie():
-    """Creates a class to allow for storage of data pertaining to any number of movies
+    """Creates a class to allow for storage of data pertaining to any number of
+    movies
     Attributes:
         Film title, film genre, film release year, poster image, trailer video
     """
-    def __init__(self, movie_title, movie_genre, movie_year, poster_image, trailer_youtube):
-        """Initializes instances of the class with the attributes described above and listed below
+    def __init__(self, movie_title, movie_genre, movie_year, poster_image,
+                 trailer_youtube):
+        """Initializes instances of the class with the attributes described
+        above and listed below
         """
         self.title = movie_title
         self.genre = movie_genre
